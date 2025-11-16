31f37b660532b661c4d15f2a7d64545200a3e4f3
barakamehdi <barakamehdi3@gmail.com>
2025-11-16 01:04:28 +0100

    ready to push

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 # Generate SSL certificate if not exists
 if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
     echo "Generating SSL certificate..."
-    mkdir -p /etc/nginx/ssl
+    mkdir -p /etc/nginx/ssl /usr/local/share/ca-certificates/
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/nginx.key \
         -out /etc/nginx/ssl/nginx.crt \
         -subj "/C=MA/ST=Morocco/L=Ben Guerir/O=42/OU=42/CN=elbaraka.42.fr"
-        sudo cp /etc/nginx/ssl/nginx.crt /usr/local/share/ca-certificates/
-        sudo update-ca-certificates
+    cp /etc/nginx/ssl/nginx.crt /usr/local/share/ca-certificates/
+    update-ca-certificates
     echo "SSL certificate generated!"
 fi
 
